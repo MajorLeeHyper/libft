949833b3f122a0b03a80be7319d0d65dcd006faa
Daniel Nelson <dan.nelson001@gmail.com>
2016-12-07 16:34:32 -0800

    initail commit, everything is done, but need to quadruple verify

---
+++ ft_strsplit.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnelson <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/02 14:38:10 by dnelson           #+#    #+#             */
+/*   Updated: 2016/12/06 12:47:52 by dnelson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <strings.h>
+
+static size_t	ft_strlen_split(char *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+static size_t	ft_count_split(char *str, char c)
+{
+	size_t	i;
+	size_t	count;
+	int		yes;
+
+	count = 0;
+	i = 0;
+	yes = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+		{
+			if (yes == 1)
+			{
+				count++;
+				yes = 0;
+			}
+		}
+		else if (yes == 0)
+			yes = 1;
+		i++;
+	}
+	if (str[i - 1] != c && i != 0)
+		count++;
+	return (count);
+}
+
+static char		*ft_strcpy_split(char *dest, char *src, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && src[i] != c)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	char	**array;
+	char	*str;
+	size_t	i[3];
+
+	if (!s)
+		return (NULL);
+	i[0] = 0;
+	i[2] = 0;
+	i[1] = ft_count_split((char*)s, c);
+	if (!(array = (char**)malloc(sizeof(char*) * (i[1] + 1))))
+		return (NULL);
+	while (i[0] < i[1])
+	{
+		while (s[i[2]] == c)
+			i[2]++;
+		if (!(str = ft_strnew(ft_strlen_split((char*)&s[i[2]], c))))
+			return (NULL);
+		array[i[0]] = str;
+		ft_strcpy_split(str, (char*)&s[i[2]], c);
+		while (s[i[2]] != c)
+			i[2]++;
+		i[0]++;
+	}
+	array[i[0]] = NULL;
+	return (array);
+}
